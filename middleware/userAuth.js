e45c35fdce08f0befd79904c36ebbb56f319ac12
morlabhaskar <morlabhaskar306@gmail.com>
2025-04-03 18:42:32 +0530

    After Getting User Data API

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -11,13 +11,15 @@ const userAuth = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     if(decoded.id) {
-        req.body.userId = decoded.id;
+        // req.body.userId = decoded.id;
+        req.user = { userId: decoded.id }; 
     }
     else{
         res.json({ success: false, message: "Unauthorized User" });
     }
     next();
   } catch (error) {
+    console.log(error)
     return res.json({ success: false, message: "Invalid Token" });
   }
 };
