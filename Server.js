cec09c3d003d110a08700e30ae4c7046741037b7
morlabhaskar <morlabhaskar306@gmail.com>
2025-04-04 09:12:05 +0530

    Add Client URL in CORS

--- Server.js
+++ Server.js
@@ -14,9 +14,11 @@ const port = process.env.PORT || 4000;
 connectDB();
 
 //middlewares
+const allowedOrigins = ['http://localhost:5173'];
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials:true}))
+app.use(cors({origin:allowedOrigins,credentials:true}))
 
 //API end points
 app.get('/',(req,res) => {
